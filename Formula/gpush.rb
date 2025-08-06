d924897b2a246341138531641ef768da1781ab12
Danilo D'Antoni <thomasd@danilo.homenet.telecomitalia.it>
2025-08-06 16:12:38 +0200

    Fix final SHA256 after tag reset

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -2,7 +2,7 @@ class Gpush < Formula
   desc "Push your Git repo to multiple remotes and branches interactively"
   homepage "https://github.com/ThomasDanilo96/brew-gpush"
   url "https://github.com/ThomasDanilo96/brew-gpush/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b2dec87bcf88a63a07f845cd3f6de38c6e5a294f47f932717679c067d752be59"
+  sha256 "788fc5b0fff0107e17f0b9ae38581e4426de47221061b374581b59b5286397aa"
   version "1.0.0"
 
   def install
