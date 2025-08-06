8cbbac68f4b5eba898a742e0a90998f3cad9f971
Danilo D'Antoni <thomasd@danilo.homenet.telecomitalia.it>
2025-08-06 16:28:39 +0200

    Fix SHA256 to match commit d924897b

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -1,9 +1,10 @@
 class Gpush < Formula
   desc "Push your Git repo to multiple remotes and branches interactively"
-  homepage "https://github.com/ThomasDanilo96/brew-gpush"
-  url "https://github.com/ThomasDanilo96/homebrew-gpush/archive/refs/heads/master.tar.gz"
-  sha256 "82dcae4bf6ba4645abff5ba1be6270f8fa8d03eaf0a6b50c940ff1677b6021a3"
+  homepage "https://github.com/ThomasDanilo96/homebrew-gpush"
+  url "https://raw.githubusercontent.com/ThomasDanilo96/homebrew-gpush/master/gpush.sh"
   version "1.0.0"
+  sha256 "<DA_CALCOLARE>"
+  license "MIT"
 
   def install
     bin.install "gpush.sh" => "gpush"
