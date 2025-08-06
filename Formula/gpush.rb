0a47e40691d1eee4405d0ed0a74a00b23120dae0
Danilo D'Antoni <thomasd@danilo.homenet.telecomitalia.it>
2025-08-06 16:20:39 +0200

    Fix: use correct branch (master) and SHA256

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -2,7 +2,7 @@ class Gpush < Formula
   desc "Push your Git repo to multiple remotes and branches interactively"
   homepage "https://github.com/ThomasDanilo96/brew-gpush"
   url "https://github.com/ThomasDanilo96/homebrew-gpush/archive/refs/heads/master.tar.gz"
-  sha256 "da5f997bdd8defacc1a78a724ff296f071d3c2f134d43e0e502cb6853d795ec2"
+  sha256 "58f49b60d9c81bcad3820e3db7c36f7aeb793a03aa1f44cbf3e558e7cc202e9e"
   version "1.0.0"
 
   def install
