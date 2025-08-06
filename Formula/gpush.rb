ef35e0bb77f099135a8e27e09c7639754d2112df
Danilo D'Antoni <thomasd@danilo.homenet.telecomitalia.it>
2025-08-06 16:09:10 +0200

    Update SHA256 for gpush v1.0.0

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -2,7 +2,7 @@ class Gpush < Formula
   desc "Push your Git repo to multiple remotes and branches interactively"
   homepage "https://github.com/ThomasDanilo96/brew-gpush"
   url "https://github.com/ThomasDanilo96/brew-gpush/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "35a4e754826596a7e6cbe320f4760f12eea88a5ecdc9a888564391dcdaf4e8cc"
+  sha256 "b2dec87bcf88a63a07f845cd3f6de38c6e5a294f47f932717679c067d752be59"
   version "1.0.0"
 
   def install
