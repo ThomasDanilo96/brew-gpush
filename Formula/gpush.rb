5bbe6f4acada03299dea5edfae850594384cb8d4
Danilo D'Antoni <thomasd@danilo.homenet.telecomitalia.it>
2025-08-06 15:54:03 +0200

    add sha256

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -2,7 +2,7 @@ class Gpush < Formula
   desc "Push your Git repo to multiple remotes and branches interactively"
   homepage "https://github.com/ThomasDanilo96/brew-gpush"
   url "https://github.com/ThomasDanilo96/brew-gpush/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "<SHA256_PLACEHOLDER>"
+  sha256 "35a4e754826596a7e6cbe320f4760f12eea88a5ecdc9a888564391dcdaf4e8cc"
   version "1.0.0"
 
   def install
